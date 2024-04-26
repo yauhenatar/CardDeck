de40acfe63f5b20dedf64d851ad53a8f8067ff2b
Yauhen <104792306+yauhenatar@users.noreply.github.com>
2024-04-26 21:12:39 +0300

    Add files via upload
    changes functions names

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from random import shuffle, choice
+from random import choice
 
-OPERATIONS = ['big', 'small']
+DECK_LENGTH = ['big', 'small']
 
 STANDART_CARDS = {
     '6': 6,
@@ -35,42 +35,43 @@ SUITS_LIST = ['hearts', 'clubs', 'spades', 'diamonds']
 JOKER_COLORS = ['black', 'red']
 
 deck_values = list()
-operation = ''
-
-def check_operations(operations=None):
-    if operations is None:
-        return OPERATIONS
+deck_length = ''
 
 
-def check_colors(colors=None):
+def set_deck_length(deck_length=None):
+    if deck_length is None:
+        return DECK_LENGTH
+
+
+def set_colors(colors=None):
     if colors is None:
         return JOKER_COLORS
 
 
-def check_suits(suits=None):
+def set_suits(suits=None):
     if suits is None:
         return SUITS_LIST
 
 
-def check_operation():
-    global operation
-    operations = check_operations()
+def set_deck_lenght():
+    global deck_length
+    operations = set_deck_length()
     operation = input('Enter needed deck (small/big):\n').lower()
     if operation not in operations:
-        return check_operation()
+        return set_deck_lenght()
     else:
         return operation
 
 
-def check_cards(operation, cards=None):
-    if operation == 'small':
+def set_cards(deck_length):
+    if deck_length == 'small':
         return STANDART_CARDS
-    elif operation == 'big':
+    elif deck_length == 'big':
         return BIG_CARDS
 
 
 def create_standart_deck(cards, deck: list):
-    suits = check_suits()
+    suits = set_suits()
 
     for suit in suits:
         for c in cards:
@@ -80,8 +81,8 @@ def create_standart_deck(cards, deck: list):
 
 
 def create_big_deck(cards, deck: list):
-    suits = check_suits()
-    joker_colors = check_colors()
+    suits = set_suits()
+    joker_colors = set_colors()
 
     for color in joker_colors:
         for jc in cards:
@@ -98,10 +99,10 @@ def create_big_deck(cards, deck: list):
     return deck
 
 
-def create_deck() -> list:
+def create_deck():
     deck = list()
-    operation = check_operation()
-    cards = check_cards(operation)
+    operation = set_deck_lenght()
+    cards = set_cards(operation)
     if operation == 'small':
         return create_standart_deck(cards, deck)
     elif operation == 'big':
@@ -109,19 +110,19 @@ def create_deck() -> list:
 
 
 def get_value(card: str):
-    cards = check_cards(operation)
+    cards = set_cards(deck_length)
     num_card = card.split()[0]
     for key in cards:
         if num_card == key:
             return cards[key]
 
 
-def get_num_cards_to_play():
+def set_num_cards_to_play():
     return int(input('Enter count of playing cards:\n'))
 
 
 def took_random_cards(deck: list):
-    cards_to_play = get_num_cards_to_play()
+    cards_to_play = set_num_cards_to_play()
     cards_list = list()
     for i in range(cards_to_play):
         choosed = choice(deck)
@@ -150,8 +151,3 @@ def get_winners(random_cards: list):
             del values_cards[max_index]
             del random_cards[max_index]
 
-
-deck = create_deck()
-random_cards = took_random_cards(deck)
-print(random_cards)
-get_winners(random_cards)
