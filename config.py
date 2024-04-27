4b5e1af1f61f61dfd6bd0a17060cdc6e9b263fe7
Yauhen <104792306+yauhenatar@users.noreply.github.com>
2024-04-27 09:15:11 +0300

    Add files via upload
    create config to keep const values

---
+++ config.py
@@ -0,0 +1,33 @@
+DECK_LENGTH = ['big', 'small']
+
+SMALL_DECK = {
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: '10',
+    11: 'jack',
+    12: 'queen',
+    13: 'king',
+    14: 'ace'
+}
+
+BIG_DECK = {
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: '10',
+    11: 'jack',
+    12: 'queen',
+    13: 'king',
+    14: 'ace',
+    15: 'joker'
+}
+
+SUITS_LIST = ['hearts', 'clubs', 'spades', 'diamonds']
+JOKER_COLORS = ['black', 'red']
--- main.py
+++ main.py
@@ -1,38 +1,5 @@
 from random import choice
-
-DECK_LENGTH = ['big', 'small']
-
-SMALL_DECK = {
-    6: '6',
-    7: '7',
-    8: '8',
-    9: '9',
-    10: '10',
-    11: 'jack',
-    12: 'queen',
-    13: 'king',
-    14: 'ace'
-}
-
-BIG_DECK = {
-    2: '2',
-    3: '3',
-    4: '4',
-    5: '5',
-    6: '6',
-    7: '7',
-    8: '8',
-    9: '9',
-    10: '10',
-    11: 'jack',
-    12: 'queen',
-    13: 'king',
-    14: 'ace',
-    15: 'joker'
-}
-
-SUITS_LIST = ['hearts', 'clubs', 'spades', 'diamonds']
-JOKER_COLORS = ['black', 'red']
+from config import *
 
 
 def set_deck_length(deck_length=None):
@@ -127,7 +94,6 @@ def get_values_of_random_cards(random_cards: list):
 
 def get_winners(random_cards: list):
     values_cards = get_values_of_random_cards(random_cards)
-    print(values_cards)
     max_value = max(values_cards)
     print('WINNER(S):')
     for el_rand, el_val in zip(random_cards, values_cards):
